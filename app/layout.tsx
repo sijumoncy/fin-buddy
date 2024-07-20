7ee7daddc45a6519ff5ecfcf7185c63c0fb39062
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2024-07-20 15:27:35 +0530

    base project setup

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+import "./globals.css";
+
+const inter = Open_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "FinBuddy",
+  description: "Your Financial Partner",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
